da2086f82afa192b39d7ea64305e0b508a359ebb
Ccmoove <152177314+Ccmoove@users.noreply.github.com>
2024-04-12 14:08:32 +0100

    Update routes.py
    lint update

--- service/routes.py
+++ service/routes.py
@@ -60,6 +60,7 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -77,6 +78,7 @@ def list_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
@@ -84,7 +86,6 @@ def get_accounts(account_id):
     This endpoint will read an Account based the account_id that is requested
     """
     app.logger.info("Request to read an Account with id: %s", account_id)
-
     account = Account.find(account_id)
     
     if not account:
@@ -94,6 +95,7 @@ def get_accounts(account_id):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
@@ -113,6 +115,7 @@ def update_accounts(account_id):
 
 ######################################################################
 # DELETE AN ACCOUNT
+
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
