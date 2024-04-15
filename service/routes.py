a0ae1b7a171b85378bde40bfb473df95cc0540bd
{Ccmoove} <{engradeyemi@yahoo.com}>
2024-04-15 09:34:24 -0400

    security added

--- service/routes.py
+++ service/routes.py
@@ -61,6 +61,7 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -87,14 +88,14 @@ def get_accounts(account_id):
     """
     app.logger.info("Request to read an Account with id: %s", account_id)
     account = Account.find(account_id)
-    
+
     if not account:
         abort(f"Account with id [{account_id}] could not be found.", status.HTTP_404_NOT_FOUND,)
     return account.serialize(), status.HTTP_200_OK
-            
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
+
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
@@ -107,16 +108,17 @@ def update_accounts(account_id):
     account = Account.find(account_id)
     account.deserialize(request.get_json())
     account.update()
-        
+
     if not account:
         abort(f"Account with id [{account_id}] could not be found.", status.HTTP_404_NOT_FOUND)
-        
+
     return account.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # DELETE AN ACCOUNT
-
 ######################################################################
+
+
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
     """
