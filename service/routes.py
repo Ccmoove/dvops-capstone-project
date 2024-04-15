a065ba01daa404fdf526aa79852d5d975e1ac32a
Ccmoove <152177314+Ccmoove@users.noreply.github.com>
2024-04-15 12:19:41 +0100

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -88,11 +88,11 @@ def get_accounts(account_id):
     """
     app.logger.info("Request to read an Account with id: %s", account_id)
     account = Account.find(account_id)
-    
+
     if not account:
         abort(f"Account with id [{account_id}] could not be found.", status.HTTP_404_NOT_FOUND,)
     return account.serialize(), status.HTTP_200_OK
-            
+      
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
@@ -109,10 +109,10 @@ def update_accounts(account_id):
     account = Account.find(account_id)
     account.deserialize(request.get_json())
     account.update()
-        
+   
     if not account:
         abort(f"Account with id [{account_id}] could not be found.", status.HTTP_404_NOT_FOUND)
-        
+
     return account.serialize(), status.HTTP_200_OK
 
 ######################################################################
